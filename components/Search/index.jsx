3210c5f4b1e9deaca44b936255dc51d13991c51a
Phillip Johnson <phil@blstr.co>
2021-07-03 19:55:38 +1000

    added clear search

--- components/Search/index.jsx
+++ components/Search/index.jsx
@@ -1,23 +1,42 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useRecoilState, useSetRecoilState, useRecoilValue } from "recoil";
 import { getPaginationState } from "../../selectors";
 import { countriesSearchState, paginationState } from "../../atoms";
 
 const Search = () => {
+    const searchEl = useRef(null);
+
     const paginationStateValue = useRecoilValue(getPaginationState);
     const setPaginationState = useSetRecoilState(paginationState);
-    const [value, setValue] = useRecoilState(countriesSearchState);
-    const searchEl = useRef(null);
-    const SearchCountries = (e) => {
-        e.preventDefault();
-        setValue(searchEl.current.value);
+
+    const [searchValue, setSearchValue] = useRecoilState(countriesSearchState);
+
+    const [value, setValue] = useState(searchValue);
+    
+    const resetPagination = () => {
         setPaginationState({
             ...paginationStateValue,
             page: 1
         })
-    } 
+    }
+
+    const SearchCountries = (e) => {
+        e.preventDefault();
+        setSearchValue(searchEl.current.value);
+        resetPagination();
+    }
+
+    const resetSearch = (e) => {
+        e.preventDefault();
+        setValue('');
+        setSearchValue('');
+        resetPagination();
+    }
+
+
     return <form onSubmit={(e) => SearchCountries(e)}>
-        <input type="text" ref={searchEl} value={value} />
+        <input type="text" ref={searchEl} value={value} onChange={(e) => setValue(e.target.value)} />
+        <button onClick={(e) => resetSearch(e)}>Clear Search</button>
         <input type="submit" />
     </form>
 }
